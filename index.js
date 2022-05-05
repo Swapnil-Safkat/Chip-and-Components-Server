b2135d6a7df3bbbfc81c33b5fb8ce982ac3b61af
Swapnil-Safkat <shahriarswapnil50@gmail.com>
2022-05-05 18:57:03 +0600

    server ready

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Connected with Chip & Comps'));
+
+const uri = `mongodb+srv://${process.env.DB_USER
+  }:${process.env.DB_PASS
+  }@learningmongo.qf50z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db('ChipAndComps').collection('Products');
+
+    // add a new service to mongo
+    app.post('/products', async (req, res) => {
+      const result = await productCollection.insertOne(req.body);
+      res.send(result);
+    });
+
+  } finally { }
+}
+run().catch(console.dir);
+
+app.listen(port, () => console.log('Listening to port ', port));
