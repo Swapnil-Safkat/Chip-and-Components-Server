029c5477344a52890718eb4cfbdd682a69fb0c15
Swapnil-Safkat <shahriarswapnil50@gmail.com>
2022-05-08 21:05:20 +0600

    removed item

--- index.js
+++ index.js
@@ -23,8 +23,13 @@ async function run() {
   try {
     await client.connect();
     const productCollection = client.db('ChipAndComps').collection('Products');
-
     //get all items
+    app.get('/product', async (req, res) => {
+      const cursor = productCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    //get 6 items
     app.get('/products', async (req, res) => {
       const cursor = productCollection.find({});
       const services = await cursor.limit(6).toArray();
@@ -43,6 +48,25 @@ async function run() {
       const item = await productCollection.findOne(query);
       res.send(item);
     });
+    //update a product stock
+    app.put('/item/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedItem = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: updatedItem
+      };
+      const result = await productCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
+    })
+    // delete a item
+    app.delete('/item/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    })
   } finally { }
 }
 run().catch(console.dir);
